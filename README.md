4d5fb51225589456aecfa689d41611fbf5b811e4
msquare <akamsquare42@gmail.com>
2024-10-11 23:22:47 +0800

    feat:stringCat.h

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # personal_fuzztest
 a repo contains several tests using google/fuzztest
+
---
+++ my_test.cpp
@@ -0,0 +1,32 @@
+//
+// Created by msquare01 on 24-10-11.
+//
+#include "fuzztest/fuzztest.h"
+#include "gtest/gtest.h"
+#include <string>
+#include "lib/stringCat.h"
+using namespace fuzztest;
+
+
+// TestFixture: StringTest
+class StringTest {
+
+public :
+    //test the property function CatString and the usual method append
+    void TestCatString(const std::optional<string>& str1, const std::optional<string>& str2) {
+    EXPECT_EQ(CatString(str1,str2),str1.value_or("").append(str2.value_or("")));
+    }
+    //test the property function ReverseString, using std::reverse as reference.
+    void TestReverseString(const std::optional<string>& str){
+    string rev = *str;
+    std::reverse(rev.begin(),rev.end());
+        EXPECT_EQ(ReverseString(str),rev);
+    };
+};
+
+
+// register FUZZ_TEST_Fixtures
+FUZZ_TEST_F(StringTest,TestCatString)
+.WithDomains(fuzztest::String(),fuzztest::String());
+FUZZ_TEST_F(StringTest,TestReverseString)
+.WithDomains(fuzztest::String());
