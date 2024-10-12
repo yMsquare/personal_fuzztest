a3b811d26c1e96cf6282c528c18587265e455740
msquare <akamsquare42@gmail.com>
2024-10-12 22:46:07 +0800

    added Unit tests

--- lib/stringCat.h
+++ lib/stringCat.h
@@ -1,14 +1,34 @@
 #ifndef STRINGCAT_H
 #define STRINGCAT_H
 #include <string>
+#include <cstring>
 using namespace std;
 
 string CatString(const std::optional<string>& str1, const std::optional<string>& str2) {
     return  str1.value()+ str2.value();
 }
-
 string ReverseString(const std::optional<string>& str1) {
     string bs =str1.value_or("");
+    int len = bs.length();
+    for (int i = 0;i<len/2;i++) {
+        char tmp;
+        tmp = bs[len-i-1];
+        bs[len-i-1] = bs[i];
+        bs[i]  = tmp;
+    }
+    return bs;
+}
+string ReverseStringWillFail(const std::optional<string>& str1) {
+    string bs =str1.value_or("");
+    // if (str1 == "") {
+    //     return "1";
+    // }
+    if (str1 == "asdfghjklqwertyuiopzxcvbnm,.sdfghjklertyuiopdfd#$%^&*()WIGDjwfdaughw") {
+        return "1";
+    }
+    if (str1 == "E$Y%DURTFYJKVUGHOULVCRS$WERTYUI") {
+        return  *str1;
+    }
     int len = bs.length();
     for (int i = 0;i<len/2;i++) {
         char tmp;
--- my_test.cpp
+++ my_test.cpp
@@ -7,20 +7,33 @@
 #include "lib/stringCat.h"
 using namespace fuzztest;
 
+TEST(StringTestSuite,TestReverseChinese_Unit) {
+    std::string str = "上海自来水来自海上";
+    EXPECT_EQ(ReverseString(str),str);
+}
+TEST(StringTestSuite,TestReverseString_Unit) {
+    std::string str = "aabaa";
+    EXPECT_EQ(ReverseString(str),str);
+}
 
 // TestFixture: StringTest
 class StringTest {
 
-public :
-    //test the property function CatString and the usual method append
-    void TestCatString(const std::optional<string>& str1, const std::optional<string>& str2) {
-    EXPECT_EQ(CatString(str1,str2),str1.value_or("").append(str2.value_or("")));
+    public :
+        //test the property function CatString and the usual method append
+        void TestCatString(const std::optional<string>& str1, const std::optional<string>& str2) {
+        EXPECT_EQ(CatString(str1,str2),str1.value_or("").append(str2.value_or("")));
     }
     //test the property function ReverseString, using std::reverse as reference.
     void TestReverseString(const std::optional<string>& str){
-    string rev = *str;
-    std::reverse(rev.begin(),rev.end());
+        string rev = *str;
+        std::reverse(rev.begin(),rev.end());
         EXPECT_EQ(ReverseString(str),rev);
+    };
+    void TestReverseStringWillFail(const std::optional<string>& str){
+        string rev = *str;
+        std::reverse(rev.begin(),rev.end());
+        EXPECT_EQ(ReverseStringWillFail(str),rev);
     };
 };
 
@@ -29,4 +42,6 @@ public :
 FUZZ_TEST_F(StringTest,TestCatString)
 .WithDomains(fuzztest::String(),fuzztest::String());
 FUZZ_TEST_F(StringTest,TestReverseString)
+.WithDomains(fuzztest::String());
+FUZZ_TEST_F(StringTest,TestReverseStringWillFail)
 .WithDomains(fuzztest::String());
